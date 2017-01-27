5694ce39e660d73f521756ccaaebce271e9921a5
sandrobirke <sandro.birke@gmail.com>
2017-01-27 14:31:17 +0100

    interpolation

--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -6,4 +6,12 @@ import {Component} from "@angular/core";
     styleUrls: ['./app/user/user.component.css']
 })
 
-export class UserComponent{}
+export class UserComponent{
+    teaser:string = "Ich bin ein Teaser";
+    getSum(num1:number, num2:number): number{
+        return num1 + num2;
+    }
+    getDescription():string{
+        return 'Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können.';
+    }
+}
