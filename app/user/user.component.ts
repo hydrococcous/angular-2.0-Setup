fe47a7137c6ebb70f1b3b21f56fe52f806614059
sandrobirke <sandro.birke@gmail.com>
2017-01-30 17:25:04 +0100

    bind events, classes, attributes

--- app/user/user.component.ts
+++ app/user/user.component.ts
@@ -1,15 +1,34 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'user',
+    styles: [`
+        .hide{
+            display: none;
+        }
+        .img{
+            margin: 0 5px 5px 0;
+            float: left;
+        }
+        P:after{
+            content: "";
+            display: block;
+            height: 0;
+            clear: both;
+        }
+        BUTTON{
+            display: block;
+            width: 100%;
+        }
+    `],
     template: `
         <section>
-            <h3>{{username}}</h3>
+            <h3>{{username}} <small>{{getSum(13,12)}}</small></h3>
             <p>
+            <img [src]="imgUrl" alt="" class="img" [class.hide]="hidden" [attr.aria-label]="label" />
             {{getDescription()}}
             </p>
-            <p>
-            {{getSum(13,12)}}
-            </p>
+            <button [attr.disabled]="disabled" (click)="onClick($event)">ready to Click!</button>
+            <input type="text" (keydown)="onKeyDown($event)" />
         </section>`
 })
 
@@ -19,6 +38,10 @@ export class UserComponent{
     }
 
     username:string = "Sandro Birke";
+    label:string = "Userimage";
+    hidden:boolean = false;
+    disabled:any = null;
+    imgUrl:string = "https://placeholdit.imgix.net/~text?txtsize=15&txt=100%C3%97100&w=100&h=100";
 
     getSum(num1:number, num2:number):number{
         return num1 + num2;
@@ -26,6 +49,16 @@ export class UserComponent{
 
     getDescription():string{
         return "Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn," +
-               "es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können.";
+               "es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können." +
+               "Auch gibt kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können.";
+    }
+
+    onClick(evt:Event):void{
+        this.disabled = true;
+        console.log('deactivated: ', evt);
+    }
+
+    onKeyDown(evt:Event):void{
+        console.log('key down: ', evt);
     }
 }
