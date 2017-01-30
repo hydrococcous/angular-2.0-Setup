5efc3912f7f40af89da1cb15d217bb959b422bbb
sandrobirke <sandro.birke@gmail.com>
2017-01-30 16:37:14 +0100

    import CommonModule in user.module.ts (for use NgIf etc)

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from "@angular/core";
 import {UserComponent} from "./user.component";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
+    imports: [CommonModule],
     declarations: [UserComponent],
     exports: [UserComponent]
 })
