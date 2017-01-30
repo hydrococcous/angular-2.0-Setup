d3c87e0d49e7556cc573ad246bb2b11bfbc5870a
sandrobirke <sandro.birke@gmail.com>
2017-01-30 14:10:39 +0100

    bootstrap with initial module "AppModule"

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app.module");
 /**
- * Starte die Anwendung mit dem Modul "AppModule"
+ * Startet die Anwendung mit dem Modul "AppModule"
  */
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,6 @@ import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"
 import {AppModule} from './app.module'
 
 /**
- * Starte die Anwendung mit dem Modul "AppModule"
+ * Startet die Anwendung mit dem Modul "AppModule"
  */
 platformBrowserDynamic().bootstrapModule( AppModule );
